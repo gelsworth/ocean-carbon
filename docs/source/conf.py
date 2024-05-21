9e123b56f07a1871d0b7e72d4cd58a1d4de7be3d
Geneviève Elsworth <46970446+gelsworth@users.noreply.github.com>
2024-05-21 14:38:09 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ocean-carbon'
+copyright = '2024, Elsworth'
+author = 'Elsworth'
 
 release = '0.1'
 version = '0.1.0'
